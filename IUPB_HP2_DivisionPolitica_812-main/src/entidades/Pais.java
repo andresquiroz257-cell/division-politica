156780a17f22374f9343269f80d056765ad31ac7
andres <andres.quiroz257@pascualbravo.edu.co>
2025-09-24 13:30:38 -0500

    .

---
+++ IUPB_HP2_DivisionPolitica_812-main/src/entidades/Pais.java
@@ -0,0 +1,49 @@
+package entidades;
+
+public class Pais {
+ private int id;
+ private String nombre;
+ private String continente;
+ private String tipoRegion;
+ private String coddigoAlfa2;
+ private String codigoAlfa3;
+ 
+ public int getId() {
+    return id;
+ }
+ public void setId(int id) {
+    this.id = id;
+ }
+ public String getNombre() {
+    return nombre;
+ }
+ public void setNombre(String nombre) {
+    this.nombre = nombre;
+ }
+ public String getContinente() {
+    return continente;
+ }
+ public void setContinente(String continente) {
+    this.continente = continente;
+ }
+ public String getTipoRegion() {
+    return tipoRegion;
+ }
+ public void setTipoRegion(String tipoRegion) {
+    this.tipoRegion = tipoRegion;
+ }
+ public String getCoddigoAlfa2() {
+    return coddigoAlfa2;
+ }
+ public void setCoddigoAlfa2(String coddigoAlfa2) {
+    this.coddigoAlfa2 = coddigoAlfa2;
+ }
+ public String getCodigoAlfa3() {
+    return codigoAlfa3;
+ }
+ public void setCodigoAlfa3(String codigoAlfa3) {
+    this.codigoAlfa3 = codigoAlfa3;
+ }
+
+ 
+}
---
+++ IUPB_HP2_DivisionPolitica_812-main/src/servicios/paisServicio.java
@@ -0,0 +1,13 @@
+package servicios;
+
+import java.util.List;
+
+import entidades.Pais;
+
+public class paisServicio {
+
+    public static List <Pais> cargarDatos() {
+        
+        return null;
+    }
+}
